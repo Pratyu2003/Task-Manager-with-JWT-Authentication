cf9c785e5276b07cd7f80723a23e0a5fdbfb5e0e
Pratyush Patidar <patidarpratyush50@gmail.com>
2023-05-19 14:12:57 +0530

    fixed node engine

--- data/database.js
+++ data/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-export const connectDB = () =>{
-    mongoose.connect("mongodb://127.0.0.1:27017", {
-    dbName: "backendTodo",
-}).then(() => console.log("Database Connected")).catch((e) => console.log(e));
+export const connectDB = () => {
+    mongoose.connect(process.env.MONGO_URI, {
+        dbName: "backendTodo",
+    }).then((c) => console.log(`Database Connected ${c.connection.host}`)).catch((e) => console.log(e));
 };
