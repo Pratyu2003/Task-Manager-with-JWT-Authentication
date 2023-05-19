0fcfc7c5f86d1b4abd22e72fc4883a87f540d913
Pratyush Patidar <91794200+Pratyu2003@users.noreply.github.com>
2023-05-19 14:04:27 +0530

    Update database.js

--- data/database.js
+++ data/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = () =>{
-    mongoose.connect("mongodb://127.0.0.1:27017", {
+    mongoose.connect(process.env.MONGO_URI, {
     dbName: "backendTodo",
 }).then(() => console.log("Database Connected")).catch((e) => console.log(e));
 };
